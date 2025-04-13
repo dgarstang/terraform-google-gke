3a59a6c8b50fd9a784660da48ecaa1fa5988e0fb
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-13 18:26:25 +1000

    ...

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "project_id" {
+  type = string
 }
 
 variable "gke_cluster_name" {
-} 
+  type = string
+}
 
 variable "region" {
+  type = string
 }
 
 variable "deletion_protection" {
+  type    = bool
   default = false
 }
